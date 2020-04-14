4c9ea54622b8141ce9700e2f1434dd54fa3209be
pvsteja <tejapathri6@gmail.com>
2020-04-14 22:27:58 +0530

    Created signatures for three methods in LRUCache class

--- LRUCache.py
+++ LRUCache.py
@@ -2,3 +2,12 @@ class LRUCache:
     def __init__(self, capacity) :
         self.capacity = capacity
         self.li = []
+    
+    def put(self, value):
+        pass
+    
+    def get(self, index):
+        pass
+    
+    def get_cache(self):
+        pass
