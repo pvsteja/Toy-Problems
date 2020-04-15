4e20a027c720195baa242c946b80f4c3092a2a83
pvsteja <tejapathri6@gmail.com>
2020-04-15 10:33:36 +0530

    LRU Completed

--- LRUCache.py
+++ LRUCache.py
@@ -4,14 +4,26 @@ class LRUCache:
         self.li = []
     
     def put(self, value):
-        pass
+        if len(self.li) < self.capacity :
+            if not value in self.li :
+                self.li.append(value)
+        if len(self.li) >= self.capacity :
+            if not value in self.li :
+                self.li.remove(value)
+                self.li.append(value)
+        else :
+            self.li.remove(value)
+            self.li.append(value)
     
     def get(self, index):
-        if index >len(self.li):
+        if index > len(self.li):
             return -1
         else :
             ind = self.li[index]
-            
+            self.li.remove(self.li[index])
+            self.li.append(ind)
+            return ind
+
     
     def get_cache(self):
         return self.li
--- Test.py
+++ Test.py
@@ -5,15 +5,18 @@ def main():
     lru.put(4)
     lru.put(2)
 
-    assert lru.get_cache() == {4, 2}, "put method failed"
+    assert lru.get_cache() == [4, 2], "put method failed"
     print("put method passed")
 
     assert lru.get(3) ==  -1,"put method failed"
     print("put method passed")
     lru.put(5)
     lru.put(4)
-    assert lru.get(3) ==  2,"put method failed"
+    assert lru.get(2) ==  4,"put method failed"
     print("put method passed")
-    assert lru.get_cache() == {2 5, 4}, "put method failed"
+    assert lru.get_cache() == [2, 5, 4], "put method failed"
+    print("get_cache passed") 
     print(" LRU completed")
 
+if __name__ == "__main__":
+    main()
